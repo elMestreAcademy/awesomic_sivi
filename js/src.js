f6b3c4d2b7bde427015a11cfbe9c2d7414db2e94
EL-NITROSO\johan <johanjzr@gmail.com>
2023-07-18 13:08:33 +0200

    se añade src a carpeta js

--- js/src.js
+++ js/src.js
@@ -1,7 +1,6 @@
 document.getElementsByClassName("boton-style")[0].addEventListener('click', cambia_estilo_1)
 document.getElementsByClassName("boton-style")[1].addEventListener('click', cambia_estilo_2)
 
-
 function cambia_estilo_1() {
     document.getElementById('estilos').href = '/css/style1.css';
 }
